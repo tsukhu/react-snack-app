645c8256bf5954d0aa7800f45ff5a654b89ccd13
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-01-24 14:17:54 -0600

    Basic ingrediants added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/AuxComponent';
+import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: { 
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render () {
         return (
             <Aux>
-                    <div> Burger </div>
+                    <Burger ingredients={this.state.ingredients}/>
                     <div> Build Controls </div>
                 </Aux>
         );
