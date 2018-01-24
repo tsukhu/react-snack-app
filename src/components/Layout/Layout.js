1bd11b4df391ab5a6496d108ba1a299f7a268634
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-01-24 11:23:36 -0600

    Root application done

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Aux from '../../hoc/AuxComponent';
+
+const layout = props => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
