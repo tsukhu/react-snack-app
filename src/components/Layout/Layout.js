ca78a1e02ce86d1426caee543c546867183c0622
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-01-24 11:39:21 -0600

    Added basic layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/AuxComponent';
+import classes from './Layout.css';
 
 const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>
+    {props.children}
+    </main>
   </Aux>
 );
 
