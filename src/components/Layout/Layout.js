9d6570ad0ab78b4620a9ba81e962d09dcc0b8c78
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-01-25 10:20:58 -0600

    Side Draw added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/AuxComponent';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>
-    {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
-export default layout;
+ sideDrawerClosedHandler = () => {
+  this.setState({showSideDrawer: false});
+ }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer 
+        closed={this.sideDrawerClosedHandler}
+        open={this.state.showSideDrawer}/>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,10 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
-        <nav>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
