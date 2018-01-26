a75a42c5dd02b211a7235cdae4385d68897e261d
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-01-26 07:32:35 -0600

    interceptor unmount added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
+ 
   render() {
     return (
       <div>
