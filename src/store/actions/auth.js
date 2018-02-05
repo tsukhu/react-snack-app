1b979c58f2c47c7a783fb0b5912427b0fec46652
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-02-05 11:29:09 +0530

    Signout added

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,7 +7,7 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (token,userId) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
     idToken: token,
@@ -19,6 +19,19 @@ export const authFail = error => {
   return {
     type: actionTypes.AUTH_FAIL,
     error: error
+  };
+};
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
   };
 };
 
@@ -39,9 +52,8 @@ export const auth = (email, password, isSignup) => {
     axios
       .post(url, authData)
       .then(response => {
-        dispatch(authSuccess(
-            response.data.idToken,
-            response.data.localId));
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => {
         dispatch(authFail(err.response.data.error));
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -26,6 +26,13 @@ const authFail = (state, action) => {
     loading: false
   });
 };
+
+const authLogout = (state, action) => {
+  return updateObject(this.state, {
+    token: null,
+    userId: null
+  });
+};
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
@@ -34,6 +41,7 @@ const reducer = (state = initialState, action) => {
       return authSuccess(state, action);
     case actionTypes.AUTH_FAIL:
       return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT: return authLogout(state,action);
     default:
       return state;
   }
