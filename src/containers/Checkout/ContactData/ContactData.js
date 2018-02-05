58bba75dea379db3acca25cbd300a8c596a670f8
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-02-05 12:12:36 +0530

    token handling added

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -109,7 +109,7 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-    this.props.onOrderBurger(order);
+    this.props.onOrderBurger(order,this.props.token);
   };
 
   checkValidity(value, rules) {
@@ -211,13 +211,14 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: orderData => dispatch(actions.purchaseBurger(orderData))
+    onOrderBurger: (orderData,token) => dispatch(actions.purchaseBurger(orderData,token))
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(
