70f221facef149be9d15aa5d1b3fb6793ca845c1
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-02-05 15:46:10 +0530

    bug fixes

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,8 @@ const sideDrawer = ( props ) => {
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(' ')} 
+            onClick={props.closed}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
