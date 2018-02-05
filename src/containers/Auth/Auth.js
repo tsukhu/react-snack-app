1a99dff8dc742ecb2c384d55bd5e57c1a29fc37b
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-02-05 10:43:34 +0530

    initial firebase login

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions';
+
 class Auth extends Component {
   state = {
     controls: {
@@ -81,7 +83,15 @@ class Auth extends Component {
         touched: true
       }
     };
-    this.setState({controls: updatedControls});
+    this.setState({ controls: updatedControls });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value
+    );
   };
 
   render() {
@@ -107,7 +117,7 @@ class Auth extends Component {
     ));
     return (
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
@@ -116,4 +126,10 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -5,3 +5,6 @@ export {
 } from './burgerBuilder';
 
 export {purchaseBurger,purchaseInit,fetchOrders} from './order';
+export {
+    auth
+} from './auth';
