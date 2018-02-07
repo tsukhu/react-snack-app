12f686b1a1f1e3bf39548c58c99000cb8644c380
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-02-07 12:40:46 +0530

    removed thunk

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+//import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 
 
@@ -27,7 +27,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk,sagaMiddleware))
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(watchAuth);
