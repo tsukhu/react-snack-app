d904ffa8a56228589390505bc6e00c0d2e32bd66
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-02-08 11:58:03 +0530

    Added build controls

--- src/containers/PizzaBuilder/PizzaBuilder.js
+++ src/containers/PizzaBuilder/PizzaBuilder.js
@@ -6,7 +6,11 @@ import * as actions from '../../store/actions';
 import Aux from '../../hoc/AuxComponent/AuxComponent';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Snipper';
+
+import BuildControls from '../../components/BuildControls/BuildControls';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 export class PizzaBuilder extends Component {
   state = {
@@ -17,7 +21,45 @@ export class PizzaBuilder extends Component {
     this.props.onInitPizzaIngredients();
   }
 
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    return sum > 0;
+  }
+
+  purchaseHandler = () => {
+    if (this.props.isAuthenticated) {
+      this.setState({ purchasing: true });
+    } else {
+      this.props.onSetAuthRedirectPath('/checkout');
+      this.props.history.push('/auth');
+    }
+    
+  };
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
+
+  purchaseContinueHandler = () => {
+    this.props.onInitPurchase();
+    this.props.history.push('/checkout');
+  };
+
   render() {
+    const disabledInfo = {
+      ...this.props.ings
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+    let orderSummary = null;
     let pizza = this.props.error ? (
       <p>Ingredients can't be loaded</p>
     ) : (
@@ -28,11 +70,38 @@ export class PizzaBuilder extends Component {
       pizza = (
         <Aux>
           <Pizza ingredients={this.props.ings} />
+          <BuildControls
+            snackType='pizza'
+            ingredientAdded={this.props.onIngredientAdded}
+            ingredientRemoved={this.props.onIngredientRemoved}
+            disabled={disabledInfo}
+            purchaseable={this.updatePurchaseState(this.props.ings)}
+            price={this.props.price}
+            ordered={this.purchaseHandler}
+            isAuth={this.props.isAuthenticated}
+          />
         </Aux>
+      );
+      orderSummary = (
+        <OrderSummary
+          ingredients={this.props.ings}
+          purchaseCanceled={this.purchaseCancelHandler}
+          purchaseContinue={this.purchaseContinueHandler}
+          price={this.props.price}
+        />
       );
     }
     
-    return (<Aux>{pizza}</Aux>);
+    return (      
+    <Aux>
+      <Modal
+        show={this.state.purchasing}
+        modalClosed={this.purchaseCancelHandler}
+      >
+        {orderSummary}
+      </Modal>
+      {pizza}
+    </Aux>);
   
   }
 }
@@ -48,7 +117,11 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onInitPizzaIngredients: () => dispatch(actions.initPizzaIngredients())
+    onIngredientAdded: ingName => dispatch(actions.addPizzaIngredient(ingName)),
+    onIngredientRemoved: ingName => dispatch(actions.removePizzaIngredient(ingName)),
+    onInitPizzaIngredients: () => dispatch(actions.initPizzaIngredients()),
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
   };
 };
 
